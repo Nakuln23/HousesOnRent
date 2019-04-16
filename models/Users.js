a95e09d2b3b117f63ea83c40ce3e0ad58866e3b3
Nakul <nakulnagargade@gmail.com>
2019-04-16 14:55:32 +0530

    Created Mongoose Models for houses and users

---
+++ models/Users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema ({
+   name : {
+       type: String,
+       required: true
+   },
+   email : {
+       type: String,
+       required: true
+   },
+   password: {
+       type: String,
+       required: true
+   }
+})
+
+const Users = mongoose.model('Users', UserSchema )
+module.exports = Users
