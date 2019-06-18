1fccb7d5cb744b24a6102422a4b7476474e22db3
nakuln23 <nakulnagargade@gmail.com>
2019-06-19 02:26:51 +0530

    Prepared to deploy

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-
+const path = require('path')
 const mongoose = require('mongoose')
-const port = 5000;
+const port = process.env.PORT || 5000;
 const keys = require('./config/keys')
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -44,9 +44,15 @@ app.use((req,res,next)=>{
 })
 
 //DB Connection
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+mongoose.connect(keys.mongoURI , { useNewUrlParser: true })
 .then(() => {console.log('MongoDB is connected')})
 .catch((err) => {console.log(err)})
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 //Server
 app.listen(port, (req,res)=>{
